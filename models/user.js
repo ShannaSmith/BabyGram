318f0b2a9244fc07e18aaad1f5d012d11b0a6481
ShannaSmith <shannagjh@gmail.com>
2022-05-22 18:57:48 -0400

    create models

--- models/user.js
+++ models/user.js
@@ -9,6 +9,8 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, lowercase: true, unique: true },
     password: String,
     photoUrl: String, // string from aws!
+    accessCode: String,
+    bio: String
   },
   {
     timestamps: true,
